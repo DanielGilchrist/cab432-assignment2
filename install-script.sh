48eb688620079efdbd0657ab6dfc7b870b468e6e
Daniel Gilchrist <danniieelg@gmail.com>
2017-10-25 03:39:44 +1000

    Updated script
    Now installs Node, GCC, Make, and installs some other things in alternative ways

--- install-script.sh
+++ install-script.sh
@@ -1,14 +1,27 @@
 #!/bin/bash  
 
+# install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+
+# install gcc
+apt-get install python-software-properties
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install gcc-4.8
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+
+# install make
+apt-get install make
+
 # install zeromq
-wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
+add-apt-repository ppa:chris-lea/zeromq
+apt-get update
 apt-get install libzmq3-dev
+ldconfig
 
 # install libevent
 apt-get install libevent-dev
-
-# run ldconfig
-ldconfig
 
 # install pip if not already installed
 apt-get install python-setuptools
@@ -21,6 +34,7 @@ pip install pyzmq
 # install zerorpc
 pip install zerorpc
 
-# install node module
+# install node modules
+npm config set unsafe-perm=true
 npm install --save zerorpc
-npm install zmq --save --unsafe-perm
+npm install zmq --save
