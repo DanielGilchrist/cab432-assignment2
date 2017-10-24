51596a828a07a61de47ff7ee7fcf00654529a94f
Daniel Gilchrist <danniieelg@gmail.com>
2017-10-25 02:25:29 +1000

    Added code so node can communicate with python on another server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let zerorpc = require('zerorpc');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let client = new zerorpc.Client();
+  client.connect("tcp://127.0.0.1:4242");
+
+  client.invoke("hello", "World!", function(error, res, more) {
+    console.log(res);
+  });
+
   res.render('index', { title: 'Express' });
 });
 
