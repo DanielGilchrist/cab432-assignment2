893bdd82ebf478b253579c21c6d3f863ebddba5a
Daniel Gilchrist <danniieelg@gmail.com>
2017-10-25 03:50:03 +1000

    Updated script
    Removed unnecessary second install of pyzmq using pip and added -y flags when installing using apt-get

--- install-script.sh
+++ install-script.sh
@@ -5,28 +5,27 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
 # install gcc
-apt-get install python-software-properties
+apt-get install -y python-software-properties
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install gcc-4.8
+apt-get install -y gcc-4.8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
 # install make
-apt-get install make
+apt-get install -y make
 
 # install zeromq
-add-apt-repository ppa:chris-lea/zeromq
+add-apt-repository ppa:chris-lea/zeromq -y
 apt-get update
-apt-get install libzmq3-dev
+apt-get install -y libzmq3-dev
 ldconfig
 
 # install libevent
-apt-get install libevent-dev
+apt-get install -y libevent-dev
 
 # install pip if not already installed
-apt-get install python-setuptools
-apt-get install python-pip
-pip install pyzmq
+apt-get install -y python-setuptools
+apt-get install -y python-pip
 
 # install zeromq bindings
 pip install pyzmq
@@ -38,3 +37,4 @@ pip install zerorpc
 npm config set unsafe-perm=true
 npm install --save zerorpc
 npm install zmq --save
+npm install
