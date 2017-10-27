f88f2b13cad797ebcb03757c62e9f31fd9cbbc63
Daniel Gilchrist <danniieelg@gmail.com>
2017-10-27 13:09:10 +1000

    Added twitter stream base

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,44 @@
 let express = require('express');
 let zerorpc = require('zerorpc');
+let twitter = require('./twitter');
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let client = new zerorpc.Client();
-  client.connect("tcp://127.0.0.1:4242");
+  client.connect("tcp://34.214.81.157:4242");
 
-  client.invoke("hello", "World!", function(error, res, more) {
-    console.log(res);
-  });
+  /*client.invoke("hello", "World!", function(error, res, more) {
+    if (error) {
+      console.log("ERROR: " + error);
+    } else {
+      console.log(res);
+    }
+  });*/
 
-  res.render('index', { title: 'Express' });
+  let twit = new twitter();
+  let twitterClient = twit.client;
+
+  let stream = twitterClient.stream('statuses/filter', { track: 'trump' }, (stream) => {
+    stream.on('data', (event) => {
+      client.invoke("hello", event.text, function(error, res, more) {
+        if (error) {
+          console.log("Invoke error: " + error);
+          throw error;
+        } else {
+          console.log("Text: " + event.text);
+          console.log("polarity: " + res);
+        }
+      });
+    });
+
+    stream.on('error', (error) => {
+      console.log("Stream error: " + error);
+      throw error;
+    });
+  }); 
+
+  res.render('index', { title: 'CAB432 Assignment 2' });
 });
 
 module.exports = router;
