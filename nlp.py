432de622d072fc38f95d058e98c3af0e81f32a35
MxtfV <mustafa.logan@outlook.com.au>
2017-11-01 16:05:46 +1000

    Updated the py file so it returns JSON objects

--- nlp.py
+++ nlp.py
@@ -8,6 +8,7 @@ from textblob import TextBlob
 import re
 import gevent
 import logging
+import json
 
 
 logging.basicConfig()
@@ -23,13 +24,18 @@ def strip_non_ascii(string):
     else:
         return None
 
+# Remove URLS. (The regex is from the internet.)
+def remove_url(string):
+    data = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', string)
+    return data
+
 # Remove/replace unreadable words and characters
 def clean_tweet(data):
     data = strip_non_ascii(data)
     if data is not None:
 
-        # Remove URLS. (I stole this regex from the internet.)
-        data = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', data)
+        # remove url
+        data = remove_url(data)
 
         # Fix classic tweet lingo
         data = re.sub(r'\bthats\b', 'that is', data)
@@ -60,23 +66,65 @@ def clean_tweet(data):
 
 
 
-
-def get_sentiment(string):
+# Returns a single JSON object which contains polarity and subjectivity
+#
+#  {"polarity": polarity, "subjectivity": subjectivity}
+#
+def sentiment(string):
      tweet = clean_tweet(string)
      if tweet is not None:
          text_blob = TextBlob(tweet)
          # add load by trying to correct the spelling
          text_blob = text_blob.correct()
-         return text_blob.sentiment.polarity
+         json_data = {"polarity": text_blob.sentiment.polarity, "subjectivity": text_blob.sentiment.subjectivity}
+         data = json.dumps(json_data)
+         return data
      else:
          return -2
 
+
+
+
+
+# Returns the number of noun_phrases appearance in JSON format
+# { "words" : [
+#               {"text" : phrase, "size": count },
+#               {"text" : phrase, "size": count }
+#             ]
+# }
+def get_phrases(string):
+    tweet = clean_tweet(string)
+    dict_array = []
+    output_dict = {}
+    if tweet is not None:
+        blob = TextBlob(tweet)
+        
+        for phrase in blob.noun_phrases:
+            dictionary = {}
+            dictionary['text'] = phrase
+            dictionary['size'] = blob.noun_phrases.count(phrase)
+            dict_array.append(dictionary)
+            
+        output_dict['words'] = dict_array
+        json_data = json.dumps(output_dict)
+        return json_data
+    else:
+        return '[]'
+
+
 # ZeroRPC class
-class HelloRPC(object):
-    def hello(self, string):
-        score = get_sentiment(string)
+class RPC(object):
+    def get_sentiment(self, string):
+        analysis = sentiment(string)
         gevent.sleep(0)
-        return "%f" % score
-s = zerorpc.Server(HelloRPC())
+        return analysis
+
+    def get_entities(self, string):
+        json_object = get_phrases(string)
+        gevent.sleep(0)
+        return json_object
+        
+        
+s = zerorpc.Server(RPC())
 s.bind("tcp://0.0.0.0:4242")
 s.run()
